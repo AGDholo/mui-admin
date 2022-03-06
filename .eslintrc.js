44a784cc7051f9025c9b9b4302f9d7867f53969a
AGDholo <agdholo@gmail.com>
2022-03-06 23:49:18 +0800

    chore: remove charts

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
     'react/jsx-filename-extension': [
       2,
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,6 @@
 import { CallMadeOutlined } from '@mui/icons-material';
-import Chart from 'react-apexcharts';
 
 import {
-  Box,
   Card,
   CardContent,
   Grid,
@@ -10,79 +8,7 @@ import {
   ThemeProvider,
   Typography,
 } from '@mui/material';
-import React from 'react';
-import { blue } from '@mui/material/colors';
 import darkTheme from '../themes/dark.theme';
-
-function DemoChart() {
-  const state = {
-    options: {
-      stroke: {
-        curve: 'smooth',
-      },
-      colors: [blue[500], blue[400]],
-      tooltip: {
-        enabled: false,
-      },
-      grid: {
-        show: false,
-      },
-      chart: {
-        toolbar: {
-          show: false,
-        },
-        id: 'basic-bar',
-        dropShadow: {
-          enabled: true,
-          top: 1,
-          left: 2,
-          blur: 3,
-          color: '#000',
-          opacity: 0.35,
-        },
-      },
-      fill: {
-        type: 'gradient',
-      },
-      yaxis: {
-        labels: {
-          show: false,
-        },
-      },
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
-        labels: {
-          show: false,
-        },
-        axisBorder: {
-          show: false,
-        },
-        axisTicks: {
-          show: false,
-        },
-      },
-    },
-
-    series: [
-      {
-        name: 'series-1',
-        data: [30, 40, 45, 50, 49, 60, 70, 91],
-      },
-    ],
-  };
-
-  return (
-    <Box height="100px" width="100%">
-      <Chart
-        options={state.options as any}
-        series={state.series}
-        type="line"
-        width="100%"
-        height="100%"
-      />
-    </Box>
-  );
-}
 
 function Home() {
   return (
@@ -126,7 +52,6 @@ function Home() {
                 $1,837.32
               </Typography>
             </CardContent>
-            <DemoChart />
           </Card>
         </ThemeProvider>
       </Grid>
