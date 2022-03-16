ded4ba33c3405ad92d80ee3d81ac449134c3b4e1
AGDholo <agdholo@gmail.com>
2022-03-16 23:42:10 +0800

    feat: add analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        visualizer(() => ({
+          filename: 'analyzer/analyzer.html',
+          gzipSize: true,
+        })),
+      ],
+    },
+  },
 });
