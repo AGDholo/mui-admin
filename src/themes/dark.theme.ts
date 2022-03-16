57ebd21a743232484034f11867cb0711d9cf36ea
AGDholo <agdholo@gmail.com>
2022-03-17 00:01:50 +0800

    chore(theme): add dark theme

---
+++ src/themes/dark.theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#020e25',
+      paper: '#020e25',
+    },
+  },
+});
+
+export default darkTheme;
